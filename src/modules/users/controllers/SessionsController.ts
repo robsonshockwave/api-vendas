16b1dedc5a07c1c85113c011649e2c3e0a0b194d
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-04 22:30:34 -0300

    service session, controller e rota

---
+++ src/modules/users/controllers/SessionsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import CreateSessionsService from '../services/CreateSessionsService';
+
+export default class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const createSession = new CreateSessionsService();
+
+    const { email, password } = request.body;
+
+    const user = await createSession.execute({ email, password });
+
+    return response.json(user);
+  }
+}
