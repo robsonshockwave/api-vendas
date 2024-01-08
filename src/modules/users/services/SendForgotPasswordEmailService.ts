3c83966ef6b2c8d573e1c12af03ffdf91db4c24a
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-07 22:55:28 -0300

    config handlebars e etherial para envio de email html

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -24,8 +24,18 @@ class SendForgotPasswordEmailService {
     // console.log(token);
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de senha',
+      templateData: {
+        template: `Olá {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
     });
   }
 }
