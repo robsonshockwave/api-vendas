13ce6073c413f9c99161c13e820270526d802b53
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-16 21:30:35 -0300

    service create order

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,5 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
 
 @EntityRepository(Product)
 export class ProductsRepository extends Repository<Product> {
@@ -12,5 +16,18 @@ export class ProductsRepository extends Repository<Product> {
     });
 
     return product;
+  }
+
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    // The method find is a method from the Repository class
+    const productIds = products.map(product => product.id);
+
+    const existsProducts = await this.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return existsProducts;
   }
 }
