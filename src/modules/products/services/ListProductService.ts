038a412734fc88ae10dcdb74c90c28a3f27f4c6a
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-18 22:14:08 -0300

    configurando o redis cache para lista de produtos

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,11 +1,23 @@
 import { getCustomRepository } from 'typeorm';
 import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
 import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductsRepository);
-    const products = await productsRepository.find();
+
+    const redisCache = new RedisCache();
+
+    let products = await redisCache.recover<Product[]>(
+      'api-vendas-PRODUCT_LIST',
+    );
+
+    if (!products) {
+      products = await productsRepository.find();
+
+      await redisCache.save('api-vendas-PRODUCT_LIST', products);
+    }
 
     return products;
   }
--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -10,14 +10,27 @@ export default class RedisCache {
 
   public async save(key: string, value: any): Promise<void> {
     console.log(key, value);
+
+    await this.client.set(key, JSON.stringify(value));
   }
 
-  /*   public async recover<T>(key: string): Promise<T | null> {
+  public async recover<T>(key: string): Promise<T | null> {
     console.log(key);
-    return null;
-  } */
+
+    const data = await this.client.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
 
   public async invalidate(key: string): Promise<void> {
     console.log(key);
+
+    await this.client.del(key);
   }
 }
