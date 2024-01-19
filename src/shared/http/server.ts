e049edecde29c5c014b92bed8ddab880d3763184
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-18 23:32:06 -0300

    rate limiter

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -8,12 +8,16 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import uploadConfig from '@config/upload';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use(rateLimiter);
+
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
