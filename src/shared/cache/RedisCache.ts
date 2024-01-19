8596db1fe9588974f9f658adb542b8399e71aeb1
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-18 21:50:32 -0300

    classe redis

---
+++ src/shared/cache/RedisCache.ts
@@ -0,0 +1,23 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+
+export default class RedisCache {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    console.log(key, value);
+  }
+
+  /*   public async recover<T>(key: string): Promise<T | null> {
+    console.log(key);
+    return null;
+  } */
+
+  public async invalidate(key: string): Promise<void> {
+    console.log(key);
+  }
+}
