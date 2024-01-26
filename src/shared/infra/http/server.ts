b0e0f4c144f91fccaff4447e8af5fb44bfd9243c
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-25 22:02:20 -0300

    alterações na config do typeorm, update para a versão 3

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,47 +1,11 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import { errors } from 'celebrate';
-import routes from './routes';
-import AppError from '@shared/errors/AppError';
-import '@shared/infra/typeorm';
-import '@shared/container';
-import uploadConfig from '@config/upload';
-import rateLimiter from './middlewares/rateLimiter';
+import { app } from './app';
 
-const app = express();
+import { dataSource } from '../typeorm';
 
-app.use(cors());
-
-app.use(express.json());
-
-app.use(rateLimiter);
-
-app.use('/files', express.static(uploadConfig.directory));
-app.use(routes);
-
-app.use(errors());
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        error: 'error',
-        message: error.message,
-      });
-    }
-
-    console.error(error);
-
-    return response.status(500).json({
-      error: 'error',
-      message: 'Internal server error',
-    });
-  },
-);
-
-app.listen(3333, () => {
-  console.log('Server started on port 3333! 🏆');
+dataSource.initialize().then(() => {
+  const server = app.listen(process.env.PORT || 3333, () => {
+    console.log('Server started on port 3333! 🏆');
+  });
 });
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,41 @@
-import { createConnection } from 'typeorm';
+import { DataSource } from 'typeorm';
 
-createConnection();
+import User from '@modules/users/infra/typeorm/entities/User';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+import Order from '@modules/orders/infra/typeorm/entities/Order';
+import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
+import Product from '@modules/products/infra/typeorm/entities/Product';
+import { CreateProducts1702433345820 } from './migrations/1702433345820-CreateProducts';
+import { CreateUsers1704335887083 } from './migrations/1704335887083-CreateUsers';
+import { CreateUserTokens1704579331629 } from './migrations/1704579331629-CreateUserTokens';
+import { CreateCustomers1704845271590 } from './migrations/1704845271590-CreateCustomers';
+import { CreateOrders1704894963904 } from './migrations/1704894963904-CreateOrders';
+import { AddCustomerIdToOrders1704895293063 } from './migrations/1704895293063-AddCustomerIdToOrders';
+import { CreateOrdersProducts1704896271728 } from './migrations/1704896271728-CreateOrdersProducts';
+import { AddOrderIdToOrdersProducts1704900552004 } from './migrations/1704900552004-AddOrderIdToOrdersProducts';
+import { AddProductIdToOrdersProducts1704901074566 } from './migrations/1704901074566-AddProductIdToOrdersProducts';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5433,
+  username: 'postgres',
+  password: 'docker',
+  database: 'apivendas',
+  entities: [User, UserToken, Customer, Order, OrdersProducts, Product],
+  migrations: [
+    CreateProducts1702433345820,
+    CreateUsers1704335887083,
+    CreateUserTokens1704579331629,
+    CreateCustomers1704845271590,
+    CreateOrders1704894963904,
+    AddCustomerIdToOrders1704895293063,
+    CreateOrdersProducts1704896271728,
+    AddOrderIdToOrdersProducts1704900552004,
+    AddProductIdToOrdersProducts1704901074566,
+  ],
+});
+
+// Criar uma migration
+// npm run typeorm migration:create src/shared/infra/typeorm/migrations/NameOfMigration
