17434262a9b0ed502442845da71d5b258c20cdea
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-25 23:56:36 -0300

    entrypoint docker

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 npm install
-npm run typeorm migration:run
-# npm run ts-node-dev -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:run -- -d src/shared/infra/typeorm/index.ts
+# npm run typeorm migration:run
+npx ts-node-dev -r tsconfig-paths/register ./node_modules/typeorm/cli.js migration:run -- -d src/shared/infra/typeorm/index.ts
 npm run dev
