11762d1b3f1f25c2058fabc41c340c0dfc2137fa
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-28 22:34:24 -0300

    teste para crisar um customer

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,6 +1,18 @@
+import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
+import CreateCustomerService from './CreateCustomerService';
+
 describe('CreateCustomerService', () => {
-  it('should be able to create a new customer', () => {
-    expect(1).toBe(1);
+  it('should be able to create a new customer', async () => {
+    const fakeCustomersRepository = new FakeCustomersRepository();
+
+    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+
+    const customer = await createCustomer.execute({
+      name: 'Robson Arruda',
+      email: '',
+    });
+
+    expect(customer).toHaveProperty('id');
   });
 
   it('should not be able to create two customers with the same email', () => {
