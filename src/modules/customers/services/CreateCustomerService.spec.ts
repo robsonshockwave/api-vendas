fdfb7a36100a34708fab16cdf2da29ecc76cf783
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-01-29 21:57:23 -0300

    ajuste no teste de create customer

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -2,12 +2,16 @@ import AppError from '@shared/errors/AppError';
 import FakeCustomersRepository from '../domain/repositories/fakes/FakeCustomersRepository';
 import CreateCustomerService from './CreateCustomerService';
 
+let fakeCustomersRepository: FakeCustomersRepository;
+let createCustomer: CreateCustomerService;
+
 describe('CreateCustomerService', () => {
+  beforeEach(() => {
+    fakeCustomersRepository = new FakeCustomersRepository();
+    createCustomer = new CreateCustomerService(fakeCustomersRepository);
+  });
+
   it('should be able to create a new customer', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-
-    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
-
     const customer = await createCustomer.execute({
       name: 'Robson Arruda',
       email: 'robson@email.com',
@@ -17,10 +21,6 @@ describe('CreateCustomerService', () => {
   });
 
   it('should not be able to create two customers with the same email', async () => {
-    const fakeCustomersRepository = new FakeCustomersRepository();
-
-    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
-
     await createCustomer.execute({
       name: 'Robson Arruda',
       email: 'robson@email.com',
